e9dab27500768c9f6a6da05ffd429182d40eeabc
shanisebarona <shanisebarona@gmail.com>
2022-02-08 16:48:03 -0500

    add custom test rule

--- packages/scanner-global-library/src/factories/axe-configuration.ts
+++ packages/scanner-global-library/src/factories/axe-configuration.ts
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 
 import type { Spec } from 'axe-core';
+import axe from 'axe-core';
 
 export type AxeConfiguration = Spec;
 
@@ -21,4 +22,31 @@ export const localAxeConfiguration: AxeConfiguration = {
     // be running in an environment with sensitive local/network endpoints, so we don't
     // allow cross-origin scanning.
     allowedOrigins: ['<same_origin>'],
+    disableOtherRules: true,
+    checks: [
+        {
+            id: 'dylang',
+            options: ['dylan'],
+            evaluate:
+                'function (node, options) {\n        var lang = (node.getAttribute("lang") || "").trim().toLowerCase();\n        var xmlLang = (node.getAttribute("xml:lang") || "").trim().toLowerCase();\n        var invalid = [];\n        (options || []).forEach(function(cc) {\n          cc = cc.toLowerCase();\n          if (lang && (lang === cc || lang.indexOf(cc.toLowerCase() + "-") === 0)) {\n            lang = null;\n          }\n          if (xmlLang && (xmlLang === cc || xmlLang.indexOf(cc.toLowerCase() + "-") === 0)) {\n            xmlLang = null;\n          }\n        });\n        if (xmlLang) {\n          invalid.push(\'xml:lang="\' + xmlLang + \'"\');\n        }\n        if (lang) {\n          invalid.push(\'lang="\' + lang + \'"\');\n        }\n        if (invalid.length) {\n          this.data(invalid);\n          return true;\n        }\n        return false;\n      }',
+            messages: {
+                pass: 'Good language',
+                fail: 'You mst use the DYLAN language',
+            },
+        } as axe.Check,
+    ],
+    rules: [
+        {
+            id: 'dylang',
+            metadata: {
+                description: "Ensures lang attributes have the value of 'dylan'",
+                help: "lang attribute must have the value of 'dylan'",
+            },
+            selector: 'html',
+            any: [],
+            all: [],
+            none: ['dylang'],
+            tags: ['wcag2aa'],
+        } as axe.Rule,
+    ],
 };
